7214130d6f359038acfaef188a8fe5dd9db527aa
kan327 <kannybaihaqi100@gmail.com>
2024-02-28 10:03:32 +0700

    2 commit

--- app/Http/Controllers/Api/ApiWebController.php
+++ app/Http/Controllers/Api/ApiWebController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Posts;
 use App\Models\Story;
 use Illuminate\Http\Request;
 
@@ -26,13 +27,17 @@ class ApiWebController extends Controller
 
     public function getAllPosts()
     {
-        $stories = Story::orderBy('updated_at', 'desc')->get();
-        return response()->json(['stories' => $stories]);
+        $posts = Posts::orderBy('updated_at', 'desc')->get()->map(function ($story) {
+            $story->featured_image = 'http://127.0.0.1:8000/' . $story->featured_image;
+            return $story;
+        });
+        return response()->json(['posts' => $posts]);
     }
 
     public function getPost(string $id)
     {
-        $story = Story::findOrFail($id);
-        return response()->json(['story' => $story]);
+        $Post = Posts::findOrFail($id);
+        
+        return response()->json(['Post' => $Post]);
     }
 }
